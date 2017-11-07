be16fdad14155f390f081de1024b8cef89672cda
rahul4times <tkdmaster.sharma@gmail.com>
2017-11-07 16:01:51 -0700

    adding associations

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,4 +1,32 @@
 const knex = require('../db/knex.js');
+
+const bookshelf = require('bookshelf')(knex);
+
+var User = bookshelf.Model.extend({
+  tableName: 'users';
+  friends: function(){
+    return this.hasMany(Friends);
+  }
+  posts: function(){
+    return this.hasMany(Posts);
+  }
+})
+
+var Posts = bookshelf.Model.extend({
+  tableName: 'posts';
+  user: function(){
+    return this.belongsTo(User);
+  }
+})
+
+var Friends = bookshelf.Model.extend({
+  tableName: 'friends';
+  user: function(){
+    return this.belongsTo(User);
+  }
+})
+
+
 module.exports = {
   userProfile: function(req, res){
     knex('user')
@@ -7,8 +35,25 @@ module.exports = {
         knex('about')
           .where('user_id', req.session.user)
           .then((about)=>{
-            res.render('profile', {user: user[0], aboutUser: about[0]})
+            knex('friends')
+              .where('user_id', req.session.user)
+              .then((friends)=>{
+                res.render('profile', {user: user[0], aboutUser: about[0],
+                  userFriends: friends[0]
+                })
+              })
           })
       })
   }
+
+  // userProfile: function(req, res){
+  //
+  //   knex('user')
+  //   .where('id', req.session.user)
+  //   .rightJoin('about', 'user.id', 'about.user_id')
+  //   .then((result)=>{
+  //
+  //     res.render('profile')
+  //   })
+  // }
 }
